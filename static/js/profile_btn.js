2329395d5763f28f51c97bfdefbad248ebba26be
willy14620 <willy14620@gmail.com>
2020-08-29 19:35:13 +0800

    '2020/08/29'

--- static/js/profile_btn.js
+++ static/js/profile_btn.js
@@ -8,34 +8,30 @@ var page_3 = document.querySelector("#page-3");
 btn_1.addEventListener("click", function() {
     if(page_1.style.display != "block"){
         hide();
-        show(btn_1, page_1);
+        page_1.style.display = "block";
+        btn_1.classList.add("active");
     }
 });
 
 btn_2.addEventListener("click", function() {
     if(page_2.style.display != "block"){
         hide();
-        show(btn_2, page_2);
+        page_2.style.display = "block";
+        btn_2.classList.add("active");
     }
 });
 
 btn_3.addEventListener("click", function() {
     if(page_3.style.display != "block"){
         hide();
-        show(btn_3, page_3);
+        page_3.style.display = "block";
+        btn_3.classList.add("active");
     }
 });
 
 function hide(){
     for(let i=1;i<=document.querySelectorAll("button.sidebar-item").length;i++){
-        document.querySelector("#btn-"+i).style.backgroundColor = "rgb(51 ,51 ,51)";
-        document.querySelector("#btn-"+i).style.color = "white";
+        document.querySelector("#btn-"+i).classList.remove("active")
         document.querySelector("#page-"+i).style.display = "none";
     }
-}
-
-function show(btn, page){
-    btn.style.backgroundColor = "white";
-    btn.style.color = "rgb(51, 51, 51)";
-    page.style.display = "block";
 }
