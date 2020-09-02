fdafacbcbe8510aeb1b8895a61473eb25b16e4ad
willy14620 <willy14620@gmail.com>
2020-09-03 02:16:25 +0800

    '2020/09/03'

--- website/controller.js
+++ website/controller.js
@@ -9,12 +9,25 @@ $(document).ready(function(){
         $("html,body").stop().animate({"scrollTop":0}, 1000);
     });
 
+    // ARTICLE BUTTON EVENTLISTENER
+    $(".aside-btn").click(function(){
+        var i = $(this).index();
+        $(".content-item").removeClass("active");
+        $(".content-item").eq(i).addClass("active");
+    });
+
     // WINDOW SCROLL
     $(window).scroll(function(){
         if($(this).scrollTop()>100){
             $("#go-to-top").fadeIn("fast");
         }else{
             $("#go-to-top").stop().fadeOut("fast");
+        }
+
+        if($(this).scrollTop()>70){
+            $(".header").css({"position":"fixed","background":"transparent","width":"100%"});
+        }else if($(this).scrollTop()<20){
+            $(".header").css({"position":"static","background":"","width":""});
         }
     })
 });
